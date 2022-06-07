47ec05872188e878a6b91ef9ed5547a7dc9ca136
veeran7 <veerangouda@theinfotiger.com>
2022-06-08 00:05:15 +0530

    second gif repo

--- src/App.js
+++ src/App.js
@@ -6,33 +6,27 @@ function App() {
   const [nameList, setNameList] = useState([]);
   const [giphyResult, setGiphyResult] = useState(null);
   const [search, setSearch] = useState("");
-  const [d, setd] = useState([]);
   const [selectedGif, setSelectedGif] = useState("");
   const [selectedList, setselectedList] = useState([]);
 
+  // on submitting Go button 
   const handleSubmit = () => {
     setNameList([...nameList, name]);
-    setd([]);
     setName("");
-    console.log("nameList", nameList);
+    setselectedList([...selectedList, selectedGif]);
+    setSearch("");
+    // console.log("nameList", nameList);
   };
 
-  const handleGif = () => {
-    setselectedList([...selectedList, selectedGif]);
-    // setSelectedGif([...selectedGif])
-  };
 
   const select = (item) => {
-    // console.log("first",item)
     setSelectedGif(
       giphyResult?.data.filter((val) => {
         return val.id == item;
-        // if(val.id === item){
-        // }
       })
     );
   };
-  console.log("selectedgif", selectedList);
+
   useEffect(() => {
     fetch(
       "https://api.giphy.com/v1/gifs/trending?api_key=eH3t7Sudb2TKpPpsrwrWNfKgIiGXG5de"
@@ -40,13 +34,6 @@ function App() {
       .then((response) => response.json())
       .then((result) => {
         setGiphyResult(result);
-        // console.log("result",result)
-        // console.log(
-        //   "result",
-        //   result.data.map((v) => {
-        //     return v.url;
-        //   })
-        // );
       })
       .catch((err) => {
         console.log(err.message);
@@ -56,7 +43,6 @@ function App() {
   const SearchTerm = giphyResult?.data.filter(
     (gif) => gif.title.toLowerCase().indexOf(search) > -1
   );
-  // console.log("SearchTerm", SearchTerm);
 
   return (
     <div className="App">
@@ -69,58 +55,46 @@ function App() {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          <button className="button" onClick={handleSubmit}>
-            Submit
-          </button>
         </div>
-
         <div className="gif">
           <input
             className="gifsearch"
             type="text"
-            placeholder="search gif"
+            placeholder="Search gif..."
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
-          <button className="button" onClick={handleGif}>
-            Submit gif
-          </button>
         </div>
+        <button className="button" onClick={handleSubmit}>
+          Go
+        </button>
+      </div>
+      <div className="">
+        {SearchTerm?.map((item) => {
+          return SearchTerm.length == 50 ? (
+            ""
+          ) : (
+            <img
+              key={item.images.fixed_height.url}
+              src={item.images.fixed_height.url}
+              alt="gif"
+              onClick={() => select(item.id)}
+            />
+          );
+        })}
       </div>
       <div className="">
         <div className="Result">
           {nameList.map((item) => (
-            <h4 className="h4"> {item}</h4>
+            <h4 className="h4" key={item} > {item}</h4>
           ))}
-          {/* {selectedGif.map((el) => {
-            el.images.fixed_height.url
-          })} */}
-
-          {
-            selectedList?.map((el) => {
-              <img src={el.images.fixed_height.url} alt="gg" />
-            })
-          }
-          {/* <img
-            src={selectedList?.map((el) => {
-              el.images.fixed_height.url;
-            })}
-            alt="gg"
-          /> */}
-          {/* <img src={selectedList?.map((el) => {
-            el.images.fixed_height.url
-          })}  /> */}
-          {/* {selectedList} */}
-        </div>
-        <div className="giphy">
-          {SearchTerm?.map((item) => {
-            return SearchTerm.length == 50 ? (
-              ""
-            ) : (
+          {selectedList?.map((el) => {
+            return (
               <img
-                src={item.images.fixed_height.url}
+                key={el[0].images.fixed_height.url}
+                className="gifs"
+                src={el[0].images.fixed_height.url}
                 alt="gif"
-                onClick={() => select(item.id)}
               />
             );
           })}
